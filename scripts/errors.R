4e31c96a1c809984914fd7b1aec038a8fb909cec
SiYun Zou <siyunzou@10-17-43-168.dynapool.wireless.nyu.edu>
2022-04-08 09:21:22 -0400

    add in clean error csv

--- scripts/errors.R
+++ scripts/errors.R
@@ -11,22 +11,22 @@ library(plotly)
 library(stringr)
 
 # read data 
-edu_data <- read_csv('../NISS-Dataviz/data/104.85_cleaned.csv')
+edu_data <- read_csv("../data/104.85_errors_cleaned.csv")
 
 # clean parenthesis in errors 
-edu_data <- edu_data %>%
-  mutate(col_total_error = as.numeric(gsub("[()]", "", edu_data$col_total_error)),
-         col_white_error = as.numeric(gsub("[()]", "", edu_data$col_white_error)),
-         col_black_error = as.numeric(gsub("[()]", "", edu_data$col_black_error)),
-         col_hisp_error = as.numeric(gsub("[()]", "", edu_data$col_black_error)),
-         col_asia_error = as.numeric(gsub("[()]", "", edu_data$col_asia_error)),
-         col_2ormore_error = as.numeric(gsub("[()]", "", edu_data$col_2ormore_error)),
-         hs_total_error = as.numeric(gsub("[()]", "", edu_data$hs_total_error)),
-         hs_white_error = as.numeric(gsub("[()]", "", edu_data$hs_white_error)),
-         hs_black_error = as.numeric(gsub("[()]", "", edu_data$hs_black_error)),
-         hs_hisp_error = as.numeric(gsub("[()]", "", edu_data$hs_black_error)),
-         hs_asia_error = as.numeric(gsub("[()]", "", edu_data$hs_asia_error)),
-         hs_2ormore_error = as.numeric(gsub("[()]", "", edu_data$hs_2ormore_error)))
+# edu_data <- edu_data %>%
+#   mutate(col_total_error = as.numeric(gsub("[()]", "", edu_data$col_total_error)),
+#          col_white_error = as.numeric(gsub("[()]", "", edu_data$col_white_error)),
+#          col_black_error = as.numeric(gsub("[()]", "", edu_data$col_black_error)),
+#          col_hisp_error = as.numeric(gsub("[()]", "", edu_data$col_black_error)),
+#          col_asia_error = as.numeric(gsub("[()]", "", edu_data$col_asia_error)),
+#          col_2ormore_error = as.numeric(gsub("[()]", "", edu_data$col_2ormore_error)),
+#          hs_total_error = as.numeric(gsub("[()]", "", edu_data$hs_total_error)),
+#          hs_white_error = as.numeric(gsub("[()]", "", edu_data$hs_white_error)),
+#          hs_black_error = as.numeric(gsub("[()]", "", edu_data$hs_black_error)),
+#          hs_hisp_error = as.numeric(gsub("[()]", "", edu_data$hs_black_error)),
+#          hs_asia_error = as.numeric(gsub("[()]", "", edu_data$hs_asia_error)),
+#          hs_2ormore_error = as.numeric(gsub("[()]", "", edu_data$hs_2ormore_error)))
 
 sapply(edu_data, class)
 
@@ -46,21 +46,22 @@ fig <- plot_ly(
                 '</br>error:', df$col_total_error),
 )
 
-# fig <- df %>%
-#   plot_ly(
-#     type='scatter',
-#     mode = 'markers',
-#     y = ~ n,
-#     x = ~col_total_error,
-#     markers = list(size = ~ State, sizeref = 4000, sizemode = 'area'),
-#     text = ~State,
-#     hovertemplate = paste(
-#       "<b>%{text}</b><br><br>",
-#       "error: %{x:.0%}<br>",
-#       "<extra></extra>"
-#     )
-#   )
+fig <- df %>%
+  plot_ly(
+    type='scatter',
+    mode = 'markers',
+    y = ~ n,
+    x = ~col_total_error,
+    markers = list(size = ~ State, sizeref = 4000, sizemode = 'area'),
+    text = ~State,
+    hovertemplate = paste(
+      "<b>%{text}</b><br><br>",
+      "error: %{x:.0%}<br>",
+      "<extra></extra>"
+    )
+  )
 
+# write.csv(edu_data, "../data/104.85_errors_cleaned.csv")
 
 
 
