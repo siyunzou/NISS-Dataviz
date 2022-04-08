fc51d4cda3d82499ceccd9a6a63433236bcf1aae
AnnikaKufrovich <trueblueannika@gmail.com>
2022-04-08 10:34:55 -0400

    Shiny app stage 1

--- NISS_Shiny_AK_SZ/app.R
+++ NISS_Shiny_AK_SZ/app.R
@@ -1,33 +1,55 @@
-#
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
-
+#loading packages
+library(tidyverse)
+library(geojsonio)
+library(RColorBrewer)
+library(sp)
+library(broom) 
+library(rgeos)
+library(mapproj)
+library(plotly)
 library(shiny)
+
+#Import hexbins
+hex <- geojson_read("us_states_hexgrid.geojson", what = "sp")
+
+#Reformat the 'google_name' field
+hex@data = hex@data %>% mutate(google_name = gsub(" \\(United States\\)", "", google_name))
+
+#Fortify the data to create a data format output
+hex_fortify <- tidy(hex, region = "google_name") 
+
+# read education data 
+edu_data <- read_csv('104.85_errors_cleaned.csv')
+
+sapply(edu_data, class)
+
+hex_fortify <- hex_fortify %>%
+    left_join(. , edu_data, by=c("id"="State")) 
+
+#Define centers of hexbins
+centers <- cbind.data.frame(data.frame(gCentroid(hex, byid = TRUE), id = hex@data$iso3166_2))
+
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
 
     # Application title
-    titlePanel("Old Faithful Geyser Data"),
+    titlePanel("NISS Placeholder title"),
 
     # Sidebar with a slider input for number of bins 
     sidebarLayout(
         sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
+            sliderInput("percentile",
+                        "Percentile Range:",
+                        min = 0,
+                        max = 100,
+                        value = c(0, 60))
         ),
 
         # Show a plot of the generated distribution
         mainPanel(
-           plotOutput("distPlot")
+           plotlyOutput("distPlot"), 
+           width = 6
         )
     )
 )
@@ -35,13 +57,18 @@ ui <- fluidPage(
 # Define server logic required to draw a histogram
 server <- function(input, output) {
 
-    output$distPlot <- renderPlot({
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white')
+    output$distPlot <- renderPlotly({
+        ggplotly(ggplot() + 
+            geom_polygon(data = hex_fortify, aes(fill = col_total, x = long, y = lat, group = group), 
+                         size = 0, alpha = 0.9, color = "#f7f7f7") + 
+            geom_text(data = centers, aes(x = x, y = y, label = id), 
+                      color = "#252525", size = 5) + #Add our labels
+            theme_void() +
+            scale_fill_gradient(low = "white", high = "purple", 
+                                name = "Percent Acheived Degree", 
+                                limits = c(input$percentile[1], input$percentile[2])) + 
+            ggtitle( "Percent with bachelor's or higher degree in the United States in 2019"), 
+            tooltip = "col_total")
     })
 }
 
