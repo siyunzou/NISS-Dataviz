6458938f4a983b1a4a1d62ace77db924af0bed40
AnnikaKufrovich <trueblueannika@gmail.com>
2022-04-08 19:55:45 -0400

    Added interactivity

--- NISS_Shiny_AK_SZ/app.R
+++ NISS_Shiny_AK_SZ/app.R
@@ -20,7 +20,11 @@ hex_fortify <- tidy(hex, region = "google_name")
 
 # read education data 
 College <- read_csv('coldata_cleaned.csv')
+College$black <- as.numeric(College$black)
+College$asian <- as.numeric(College$asian)
 HighSchool <- read_csv('hsdata_cleaned.csv')
+HighSchool$black <- as.numeric(HighSchool$black)
+HighSchool$asian <- as.numeric(HighSchool$asian)
 
 hex_fortify_col <- hex_fortify %>%
     left_join(. , College, by=c("id"="State")) 
@@ -41,6 +45,16 @@ ui <- fluidPage(
     # Sidebar with a slider input for number of bins 
     sidebarLayout(
         sidebarPanel(
+            selectInput("degree", "Educational Attainment:", 
+                        c("Bachelor's Degree or Higher", 
+                          "High School Degree or Higher")), 
+            selectInput("race", "Race:", 
+                        c("Total" = "total", 
+                          "White" = "white", 
+                          "Black" = "black", 
+                          "Hispanic" = "hispanic", 
+                          "Asian" = "asian", 
+                          "Two or more races" = "two or more race")), 
             sliderInput("percentile",
                         "Percentile Range:",
                         min = 0,
@@ -58,22 +72,32 @@ ui <- fluidPage(
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
-
+    datasetInput <- reactive({
+        if (input$degree == "Bachelor's Degree or Higher"){
+            dataset <- hex_fortify_col
+        }
+        else if (input$degree == "High School Degree or Higher"){
+            dataset <- hex_fortify_hs
+        }
+        return(dataset)
+    })
+    
     output$distPlot <- renderPlotly({
         ggplotly(ggplot() + 
-            geom_polygon(data = hex_fortify_col, aes(fill = total, x = long, y = lat, 
+            geom_polygon(data = datasetInput(), aes(fill = datasetInput()[[input$race]], x = long, y = lat, 
                                                      group = group, 
-                                                     text = paste0("State: ", `id`, 
+                                                     text = paste0("State: ", id, 
                                                                   "<br>Percentage (", "total", "): ", 
-                                                                  `total`, "%",
+                                                                  datasetInput()[[input$race]], "%",
                                                                   "<br>Standard error: ", 
-                                                                  `total standard error`)), 
+                                                                  datasetInput()[[paste(input$race, "standard error")]])), 
                          size = 0, alpha = 0.9, color = "#f7f7f7") + 
             theme_void() +
             scale_fill_gradient(low = "white", high = "purple", 
-                                name = "Percent Acheived", 
+                                name = "Percent (%)", 
                                 limits = c(input$percentile[1], input$percentile[2])) + 
-            ggtitle( "Percent with bachelor's or higher degree in the United States in 2019"), tooltip = "text") %>% 
+            ggtitle(paste("Percent with", tolower(input$degree), "in the United States in 2019")), 
+            tooltip = "text") %>% 
             plotly::layout(xaxis = list(title = "", 
                                         zeroline = FALSE, 
                                         showline = FALSE,
