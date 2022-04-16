1739c575b9bbb36ff9c635320cdb390c4c5c9a13
AnnikaKufrovich <trueblueannika@gmail.com>
2022-04-16 13:12:49 -0400

    Finished App!!!

--- NISS_Shiny_AK_SZ/app.R
+++ NISS_Shiny_AK_SZ/app.R
@@ -19,10 +19,10 @@ hex@data = hex@data %>% mutate(google_name = gsub(" \\(United States\\)", "", go
 hex_fortify <- tidy(hex, region = "google_name") 
 
 # read education data 
-College <- read_csv('coldata_cleaned.csv')[-1,]
+College <- read_csv('coldata_cleaned.csv')
 College$black <- as.numeric(College$black)
 College$asian <- as.numeric(College$asian)
-HighSchool <- read_csv('hsdata_cleaned.csv')[-1,]
+HighSchool <- read_csv('hsdata_cleaned.csv')
 HighSchool$black <- as.numeric(HighSchool$black)
 HighSchool$asian <- as.numeric(HighSchool$asian)
 
@@ -47,21 +47,21 @@ ui <- fluidPage(
     
     # Sidebar with a slider input for number of bins 
     sidebarLayout(
-        sidebarPanel(HTML("<b>App Introduction:</b>"), 
+        sidebarPanel(HTML("<p><b>App Introduction:</b>"), 
         HTML("<ul class=\"a\">
         <li> Select \"Educational Attainment\" and \"Race\" from the 
         dropdown menus below to update the 
-        map and standard error distribution. </li>
-        <li>Use the slider to adjust the percentile range displayed on the map**</li>
+        map and standard error distribution </li>
+        <li>Use the slider to adjust the percentile range displayed on the map&#10013; </li>
         <li>Move your cursor over the plots for more information</li>
-        <i>**Note: Both states with missing data (NA) or that are 
-             outside the percentile range will appear grey</i>
+        <p>&#10013;<i>Note: States with missing (NA) data or those that are 
+             outside the percentile range will appear gray</p></i>
              </ul>"), 
             selectInput("degree", "Educational Attainment:", 
                         c("Bachelor's Degree or Higher", 
                           "High School Degree or Higher*")), 
             selectInput("race", "Race:", 
-                        c("Total*" = "total", 
+                        c("Total**" = "total", 
                           "White" = "white", 
                           "Black" = "black", 
                           "Hispanic" = "hispanic", 
@@ -73,15 +73,16 @@ ui <- fluidPage(
                         max = 100,
                         value = c(0, 60)), 
         HTML(paste0("<b>US Overall Stats (", textOutput("race.text", inline = TRUE),"):</b> 
-             <br>Percent Achieved: ", textOutput("mean.text", inline = TRUE), "<br> ",
-                   "Standard Error: ", textOutput("se.text", inline = TRUE), "<br> <br> ")),
+        <ul class=\"a\">
+             <li>Percent Achieved: ", textOutput("mean.text", inline = TRUE), "%</li> ",
+                    "<li>Standard Error: ", textOutput("se.text", inline = TRUE), "</li> 
+                    </ul>")),
         
         HTML("<b> More Information:</b>
-             <br> <p><a href=\"https://nces.ed.gov/programs/digest/d20/tables/dt20_104.85.asp\">NCES Source Data Here</a></p>
+             <br> <p><a href=\"https://nces.ed.gov/programs/digest/d20/tables/dt20_104.85.asp\">NCES Source Data</a></p>
              <i>*High school completion includes through equivalency programs 
              such as a GED program.</i>
-             <br><i>*Total includes racial/ethnic groups not shown separately.</i>
-             <br><i>*States that have missing (NA) in data: Montana, Vermont, Wyoming </i>
+             <br><i>**Total includes all racial/ethnic groups, including those not listed in this data.</i>
 ")
         ),
         
@@ -135,17 +136,20 @@ server <- function(input, output) {
         subplot(ggplotly(ggplot() + 
                              geom_polygon(data = datasetInput(), aes(fill = datasetInput()[[input$race]], x = long, y = lat, 
                                                                      group = group, 
-                                                                     text = paste0(id, 
+                                                                     text = paste0("<b>",id, "</b>", 
                                                                                    "<br>Percentage (", input$race, "): ", 
                                                                                    datasetInput()[[input$race]], "%",
                                                                                    "<br>Standard error: ", 
-                                                                                   datasetInput()[[paste(input$race, "standard error")]])), 
+                                                                                   datasetInput()[[paste(input$race, "standard error")]], 
+                                                                                   "<br>Difference from US (%): ", 
+                                                                                   round(datasetInput3()[[input$race]] - 
+                                                                                       datasetInput()[[input$race]], 1))), 
                                           size = 0, alpha = 0.9, color = "#f7f7f7") + 
                              theme_void() +
                              scale_fill_gradient(low = "white", high = "purple", #muted purple hexcode: #9467bd
                                                  name = "Percent (%)", 
                                                  limits = c(input$percentile[1], input$percentile[2])) + 
-                             ggtitle(paste("Percent with", tolower(input$degree), "in the United States")), 
+                             ggtitle(paste("Percent with", input$degree, "in the United States")), 
                          tooltip = "text", height = 700) %>% 
                     plotly::layout(xaxis = list(title = "", 
                                                 zeroline = FALSE, 
@@ -156,12 +160,13 @@ server <- function(input, output) {
                                                 zeroline = FALSE, 
                                                 showline = FALSE,
                                                 showticklabels = FALSE, 
-                                                showgrid = FALSE)#, height = 700
+                                                showgrid = FALSE), 
+                                   hoverlabel = list(bgcolor = "white")
                     ) %>% 
                     add_annotations(x = centers$x, y = centers$y, text = centers$id, showarrow = FALSE), 
                 plot_ly(x = datasetInput2()[[error.var()]], type="box", 
                         hoverinfo = "text", 
-                        hovertext = paste(datasetInput2()$State,"<br>Standard Error:", 
+                        hovertext = paste0("<b>", datasetInput2()$State, "</b>", "<br>Standard error: ", 
                                           datasetInput2()[[error.var()]]), 
                         jitter = datasetInput2()[[error.var()]], 
                         pointpos = .1, boxpoints = 'all', name = " ") ,
